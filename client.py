b9a0a3fee029781809e5eadf1f72f0bec0576207
ren0nie0 <32780767+ren0nie0@users.noreply.github.com>
2022-08-30 15:10:24 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import requests
 class Dimagi_Client:
     def run(self):
         url = 'https://www.commcarehq.org/a/data-engineer-interview/api/v0.5/case/?type=patient&format=json'
-        r = requests.get(url, auth=('mr.brian.rennie@gmail.com', 'S0lekk79'))
+        r = requests.get(url, auth=('mr.brian.rennie@gmail.com', '#######'))
         return r.json()
 
 
